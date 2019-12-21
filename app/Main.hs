b5bd4442fea6680c465d3ee5c16afd6892559251
Marcin Czerwonka <mcmarcinoser5@gmail.com>
2019-12-22 00:23:14 +0100

    Allow limiting count of images to get Check for unknown options

--- app/Main.hs
+++ app/Main.hs
@@ -8,27 +8,38 @@ import HTTPRequests
 import System.Environment
 import Control.Concurrent.ParallelIO.Global
 import Utility
+import Control.Monad
 
 -- TODO: Get all images from all image pages
--- TODO: Extract set number of pages or images
 -- TODO: More safety checks
 
 main :: IO ()
 main = do
+  -- Get command line arguments
   _cliArguments <- getArgs
   cliArguments <- if null _cliArguments
     then printUsage >> errorWithoutStackTrace "No arguments passed"
     else getArgs
 
+  -- Check if all arguments are viable, error if they are not
+  when (fst $ hasUnknownSettings cliArguments)
+    $ printUsage >> errorWithoutStackTrace ("Unknown option " ++ snd (hasUnknownSettings cliArguments))
+
+  -- Process
   if isSinglePage $ last cliArguments
     then do -- Download single page
       downloadLink <- processImagePage $ last cliArguments
       downloadRaw downloadLink
     else do -- Download all from page
+      let maxImages = CLI.getImgCount cliArguments
       let tagString = makeTagURLPart $ last cliArguments
+
       _imagePages <- processListPage (listBaseURL ++ tagString)
-      let imagePages = if null $ filterOut (== "https:>") _imagePages
+      let _imagePageF = filterOut (== "https:>") _imagePages
+      let imagePages = if null _imagePageF
           then errorWithoutStackTrace "No posts found"
-          else filterOut (== "https:>") _imagePages
+          else case maxImages of
+            CLI.Unlimited -> _imagePageF
+            CLI.Limited limit -> take limit _imagePageF
       downloadLink <- mapM processImagePage imagePages
       parallel_ (map downloadRaw downloadLink) >> stopGlobalPool
--- src/CLI.hs
+++ src/CLI.hs
@@ -1,6 +1,7 @@
-module CLI (printUsage, isSinglePage) where
+module CLI (printUsage, isSinglePage, Count(Limited, Unlimited), getImgCount, hasUnknownSettings) where
 
 import Data.List (isPrefixOf)
+import Text.Read (readMaybe)
 
 printUsage :: IO ()
 printUsage = mapM_ putStr ["\nUsage:\n"
@@ -8,7 +9,7 @@ printUsage = mapM_ putStr ["\nUsage:\n"
   , "Tags are a comma-separated list of tags to include in the search\n"
   , "To exclude a tag, prefix it with '-', eg. blue_hair,-long_hair\n"
   , "Options:\n"
-  , "    None yet\n"]
+  , "    -c = maximum count of images to get\n"]
 
 isSinglePage :: String -> Bool
 isSinglePage s = ("https://" `isPrefixOf` s || "http://" `isPrefixOf` s) && isImgURL s
@@ -16,3 +17,26 @@ isSinglePage s = ("https://" `isPrefixOf` s || "http://" `isPrefixOf` s) && isIm
   isImgURL [] = False
   isImgURL ('&':'s':'=':'v':'i':'e':'w':_) = True
   isImgURL (_:xs) = isImgURL xs
+
+data Count = Limited Int | Unlimited
+
+getImgCount :: [String] -> Count
+getImgCount args = if findImgOption args == 0 then Unlimited else Limited $ findImgOption args
+  where
+  findImgOption [] = 0
+  findImgOption (o:os)
+    | o == "-c" = if null os
+      then errorWithoutStackTrace "Image count option was without value"
+      else case readMaybe (head os) of
+        Nothing -> errorWithoutStackTrace "Cannot convert image count value to integer"
+        Just x -> x
+    | otherwise = findImgOption os
+
+hasUnknownSettings :: [String] -> (Bool, String)
+hasUnknownSettings args = go $ take (length args - 1) args
+  where
+  go [] = (False, "")
+  go (o:os)
+    | o == "-c" = go os -- Max image count
+    | "-" `isPrefixOf` o = (True, o) -- Is option, but is not recognized
+    | otherwise = go os
